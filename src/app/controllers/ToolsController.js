6d0acc2038e507c5bbe9d0f2f0109e282394eda1
Nicolas <nicolasrenard1999@gmail.com>
2018-11-27 10:32:23 -0200

    Adicionado api.apib para documentar a api, e adicionado nova rota de mostrar a ferramenta.

--- src/app/controllers/ToolsController.js
+++ src/app/controllers/ToolsController.js
@@ -2,27 +2,43 @@ const Tool = require('../models/Tool')
 
 class ToolsController {
   async index (req, res) {
-    const { tag } = req.query
+    const { tag, title } = req.query
+
+		const filters = {}
 
     if (tag) {
-      const tools = await Tool.find({ tags: tag })
+      filters.tags = tag
+		}
 
-      return res.json(tools)
-    }
+		if (title) {
+			filters.title =  new RegExp(title, 'i')
+		}
 
-    const tools = await Tool.find()
+    const tools = await Tool.find(filters)
+
     return res.json(tools)
   }
 
   async store (req, res) {
     try {
       const tool = await Tool.create(req.body)
-
       return res.json(tool)
     } catch (err) {
       return res.status(400).json(err)
     }
   }
+
+	async show (req, res) {
+		const { id } = req.params
+
+		const tool = await Tool.findOne({ _id: id })
+
+		if (!tool) {
+			return res.status(400).json({ error: 'Tool not found' })
+		}
+
+		return res.json(tool)
+	}
 
   async destroy (req, res) {
     const { id } = req.params
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const ToolsController = require('./app/controllers/ToolsController')
 const routes = express.Router()
 
 routes.get('/tools', ToolsController.index)
+routes.get('/tools/:id', ToolsController.show)
 routes.post('/tools', ToolsController.store)
 routes.delete('/tools/:id', ToolsController.destroy)
 
